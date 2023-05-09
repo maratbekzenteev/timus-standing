1d9afa665246fcd91c6fa8547ba0cc0420717f3f
marat <maratbekzenteev@yandex.ru>
2023-05-09 18:53:12 +0300

    coroutines.py fixed once again

--- coroutines.py
+++ coroutines.py
@@ -2,8 +2,8 @@ import schedule
 import routines as r
 import time
 
-schedule.every(10).minutes.do(r.update_all_user_tasks())
-schedule.every(1).day.do(r.update_tasks())
+schedule.every(10).minutes.do(r.update_all_user_tasks)
+schedule.every(1).day.do(r.update_tasks)
 
 while True:
     schedule.run_pending()
