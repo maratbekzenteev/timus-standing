15cacc8d109a6c0436e262adc166173871c10a22
marat <maratbekzenteev@yandex.ru>
2023-05-09 13:51:42 +0300

    coroutines.py fixed

--- coroutines.py
+++ coroutines.py
@@ -1,5 +1,10 @@
 import schedule
 import routines as r
+import time
 
 schedule.every(10).minutes.do(r.update_all_user_tasks())
 schedule.every(1).day.do(r.update_tasks())
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
